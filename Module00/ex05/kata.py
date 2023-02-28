05a6e4409c9286ce791b4b346f7703a28150fb7c
dedelmir <dedelmir@student.21-school.ru>
2023-02-28 21:18:51 +0300

    ex05

--- Module00/ex05/kata.py
+++ Module00/ex05/kata.py
@@ -12,9 +12,12 @@ k02 = (3, 30, 2019, 9, 25)
 
 k03 = "The right format"
 
+k04 = (0, 4, 132.42222, 10000, 12345.67)
+
 if __name__ == '__main__':
     print(f'The 3 numbers are {str(k00)[1:-1]}')
     for key, value in k01.items():
         print(f'{key} was created by {value}')
     print(datetime(*k02[2:], *k02[:2]).strftime('%m/%d/%Y %H:%M'))
     print(f'{k03:->42}')
+    print('day_{0:0>2}, ex_{1:0>2} : {2:.2f}, {3:.2e}, {4:.2e}'.format(*k04))
